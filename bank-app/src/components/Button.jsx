97d58cab0abcbdaef1db5efe513e74ac675b1c7f
cem <cemsimseksm@gmail.com>
2022-11-23 17:14:37 +0300

    button component style finished

--- bank-app/src/components/Button.jsx
+++ bank-app/src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[16px] my-5`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
